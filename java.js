72e2a2af9e9ac69df61576b5898f2831a50c1f17
Sameeer-tech <Sameerkong42@gmail.com>
2024-11-12 12:31:16 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,60 @@
+
+
+// // OBJECT
+
+// z = 200
+// classone = ["Sameer" , "Hammad" , "Ahsan"]
+
+// Sameer = {
+// name : 'Sameer', 
+// Fathername: 'Basit',
+// phone : "03002255655",
+// address : "khi"
+// }
+
+
+
+// Hammad = {
+//     name : 'Ahsan', 
+//     Fathername: 'Basit',
+//     phone : "03002255655",
+//     address : "khi"
+//     }
+
+    
+
+
+
+// Ahsan = {
+//     name : 'Ahsan', 
+//     Fathername: 'Basit',
+//     phone : "03002255655",
+//     address : "khi"
+//     }
+
+
+
+
+//     console.log(Sameer.address , Sameer.phone , Sameer.name)
+//     console.log(Ahsan.phone)
+
+
+
+
+classtwo = [{
+
+    id : 1,
+    name : "Ahsan",
+    fathername : "Basit",
+    phone : "0312225845",
+
+},{
+
+id : 2,
+name : "Farhan",
+fathername : "Ahsaan",
+phone : "031258545",
+}]
+
+
+console.log(classtwo[0])
